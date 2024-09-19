cde3e78512b86084591fd47ede87cdf84ce4b184
Your Name <you@example.com>
2024-09-19 12:51:36 -0300

    build_apk

--- App.js
+++ App.js
@@ -1,12 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, Alert, Modal, Image, Switch } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 import * as ImagePicker from 'expo-image-picker';
 import * as MediaLibrary from 'expo-media-library';
-import Inicio from './inicio'; // Importa el componente de inicio
-
-const Stack = createStackNavigator();
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 const App = () => {
   const [hasCameraPermission, setHasCameraPermission] = useState(null);
@@ -15,7 +11,7 @@ const App = () => {
   const [modalVisible, setModalVisible] = useState(false);
   const [photoModalVisible, setPhotoModalVisible] = useState(false);
   const [darkMode, setDarkMode] = useState(false);
-  const [language, setLanguage] = useState('es'); // Idioma por defecto es español
+  const [language, setLanguage] = useState('es');
 
   const requestPermissions = async () => {
     const { status: cameraStatus } = await ImagePicker.requestCameraPermissionsAsync();
@@ -60,6 +56,7 @@ const App = () => {
 
   const handleContinue = () => {
     setPhotoModalVisible(false);
+    // Aquí puedes agregar la lógica para continuar con la aplicación
   };
 
   useEffect(() => {
@@ -100,110 +97,106 @@ const App = () => {
   };
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Inicio" screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Inicio" component={Inicio} />
-        <Stack.Screen name="MainApp">
-          {() => (
-            <View style={[styles.container, darkMode && styles.darkContainer]}>
-              <View style={styles.titleContainer}>
-                <Text style={[styles.title, darkMode && styles.darkTitle]}>Car Price</Text>
-              </View>
+    <View style={[styles.container, darkMode && styles.darkContainer]}>
+      <View style={styles.titleContainer}>
+        <Text style={[styles.title, darkMode && styles.darkTitle]}>Car Price</Text>
+        <Icon name="car" size={50} color={darkMode ? '#fff' : '#000'} />
+      </View>
 
-              <View style={styles.contentContainer}>
-                <View style={[styles.instructionBox, darkMode && styles.darkInstructionBox]}>
-                  <Text style={[styles.subtitle, darkMode && styles.darkSubtitle]}>
-                    <Text style={[styles.subtitleHeader, darkMode && styles.darkSubtitleHeader]}>
-                      {translations[language].howItWorks}
-                    </Text>{'\n\n'}
-                    <Text style={[styles.subtitleItem, darkMode && styles.darkSubtitleItem]}>
-                      1. 📸 {translations[language].takePhoto}
-                    </Text>{'\n'}
-                    {language === 'es' ? 'Abre la cámara del teléfono y toma una foto del auto.' : 'Open your phone’s camera and take a photo of the car.'}{'\n\n'}
-                    <Text style={[styles.subtitleItem, darkMode && styles.darkSubtitleItem]}>
-                      2. 📝 {translations[language].checkPrice}
-                    </Text>{'\n'}
-                    {language === 'es' ? 'Recibirás una estimación del precio del auto en base a la foto.' : 'You will receive an estimate of the car’s price based on the photo.'}
-                  </Text>
-                </View>
+      <View style={styles.contentContainer}>
+        <View style={[styles.instructionBox, darkMode && styles.darkInstructionBox]}>
+          <Text style={[styles.subtitle, darkMode && styles.darkSubtitle]}>
+            <Text style={[styles.subtitleHeader, darkMode && styles.darkSubtitleHeader]}>
+              {translations[language].howItWorks}
+            </Text>{'\n\n'}
+            <Text style={[styles.subtitleItem, darkMode && styles.darkSubtitleItem]}>
+              1. 📸 {translations[language].takePhoto}
+            </Text>{'\n'}
+            <Text style={[styles.subtitleItem, darkMode && styles.darkSubtitleItem]}>
+              {language === 'es' ? 'Abre la cámara del teléfono y toma una foto del auto.' : 'Open your phone’s camera and take a photo of the car.'}
+            </Text>{'\n\n'}
+            <Text style={[styles.subtitleItem, darkMode && styles.darkSubtitleItem]}>
+              2. 📝 {translations[language].checkPrice}
+            </Text>{'\n'}
+            <Text style={[styles.subtitleItem, darkMode && styles.darkSubtitleItem]}>
+              {language === 'es' ? 'Recibirás una estimación del precio del auto en base a la foto.' : 'You will receive an estimate of the car’s price based on the photo.'}
+            </Text>
+          </Text>
+        </View>
 
-                <TouchableOpacity style={[styles.button, darkMode && styles.darkButton]} onPress={openCamera}>
-                  <Text style={[styles.buttonText, darkMode && styles.darkButtonText]}>
-                    {translations[language].openCamera}
-                  </Text>
-                </TouchableOpacity>
-              </View>
+        <TouchableOpacity style={[styles.button, darkMode && styles.darkButton]} onPress={openCamera}>
+          <Text style={[styles.buttonText, darkMode && styles.darkButtonText]}>
+            {translations[language].openCamera}
+          </Text>
+        </TouchableOpacity>
+      </View>
 
-              <TouchableOpacity style={[styles.settingsButton, darkMode && styles.darkSettingsButton]} onPress={() => setModalVisible(true)}>
-                <Image source={require('./assets/settings.png')} style={styles.settingsIcon} />
-              </TouchableOpacity>
+      <TouchableOpacity style={[styles.settingsButton, darkMode && styles.darkSettingsButton]} onPress={() => setModalVisible(true)}>
+        <Image source={require('./assets/settings.png')} style={styles.settingsIcon} />
+      </TouchableOpacity>
 
-              <Modal
-                visible={modalVisible}
-                transparent
-                animationType="slide"
-                onRequestClose={() => setModalVisible(false)}
-              >
-                <View style={[styles.modalContainer, darkMode && styles.darkModalContainer]}>
-                  <View style={[styles.modalContent, darkMode && styles.darkModalContent]}>
-                    <TouchableOpacity style={styles.closeButton} onPress={() => setModalVisible(false)}>
-                      <Text style={styles.closeButtonText}>✖</Text>
-                    </TouchableOpacity>
-                    <Text style={[styles.modalTitle, darkMode && styles.darkModalTitle]}>{translations[language].settings}</Text>
-                    <TouchableOpacity
-                      onPress={() => changeLanguage('es')}
-                      style={[styles.languageButton, language === 'es' && styles.selectedLanguageButton]}
-                    >
-                      <Text style={styles.languageButtonText}>Español</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                      onPress={() => changeLanguage('en')}
-                      style={[styles.languageButton, language === 'en' && styles.selectedLanguageButton]}
-                    >
-                      <Text style={styles.languageButtonText}>English</Text>
-                    </TouchableOpacity>
-                    <View style={styles.switchContainer}>
-                      <Text style={[styles.switchLabel, darkMode && styles.darkSwitchLabel]}>{translations[language].darkMode}</Text>
-                      <Switch
-                        value={darkMode}
-                        onValueChange={toggleDarkMode}
-                      />
-                    </View>
-                  </View>
-                </View>
-              </Modal>
-
-              <Modal
-                visible={photoModalVisible}
-                transparent
-                animationType="slide"
-                onRequestClose={() => setPhotoModalVisible(false)}
-              >
-                <View style={styles.photoModalContainer}>
-                  <View style={styles.photoModalContent}>
-                    <Image source={{ uri: imageUri }} style={styles.image} />
-                    <View style={styles.photoModalButtonsContainer}>
-                      <TouchableOpacity style={styles.takeAnotherButton} onPress={handleTakeAnother}>
-                        <Text style={styles.takeAnotherButtonText}>{language === 'es' ? 'Tomar Otra' : 'Take Another'}</Text>
-                      </TouchableOpacity>
-                      <TouchableOpacity style={styles.continueButton} onPress={handleContinue}>
-                        <Text style={styles.continueButtonText}>{language === 'es' ? 'Continuar' : 'Continue'}</Text>
-                      </TouchableOpacity>
-                    </View>
-                  </View>
-                </View>
-              </Modal>
-
-              <View style={styles.adContainer}>
-                <Text style={styles.adText}>
-                  {translations[language].adSpace}
-                </Text>
-              </View>
+      <Modal
+        visible={modalVisible}
+        transparent
+        animationType="slide"
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View style={[styles.modalContainer, darkMode && styles.darkModalContainer]}>
+          <View style={[styles.modalContent, darkMode && styles.darkModalContent]}>
+            <TouchableOpacity style={styles.closeButton} onPress={() => setModalVisible(false)}>
+              <Text style={styles.closeButtonText}>✖</Text>
+            </TouchableOpacity>
+            <Text style={[styles.modalTitle, darkMode && styles.darkModalTitle]}>{translations[language].settings}</Text>
+            <TouchableOpacity
+              onPress={() => changeLanguage('es')}
+              style={[styles.languageButton, language === 'es' && styles.selectedLanguageButton]}
+            >
+              <Text style={styles.languageButtonText}>Español</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => changeLanguage('en')}
+              style={[styles.languageButton, language === 'en' && styles.selectedLanguageButton]}
+            >
+              <Text style={styles.languageButtonText}>English</Text>
+            </TouchableOpacity>
+            <View style={styles.switchContainer}>
+              <Text style={[styles.switchLabel, darkMode && styles.darkSwitchLabel]}>{translations[language].darkMode}</Text>
+              <Switch
+                value={darkMode}
+                onValueChange={toggleDarkMode}
+              />
             </View>
-          )}
-        </Stack.Screen>
-      </Stack.Navigator>
-    </NavigationContainer>
+          </View>
+        </View>
+      </Modal>
+
+      <Modal
+        visible={photoModalVisible}
+        transparent
+        animationType="slide"
+        onRequestClose={() => setPhotoModalVisible(false)}
+      >
+        <View style={styles.photoModalContainer}>
+          <View style={styles.photoModalContent}>
+            <Image source={{ uri: imageUri }} style={styles.image} />
+            <View style={styles.photoModalButtonsContainer}>
+              <TouchableOpacity style={styles.takeAnotherButton} onPress={handleTakeAnother}>
+                <Text style={styles.takeAnotherButtonText}>{language === 'es' ? 'Tomar Otra' : 'Take Another'}</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.continueButton} onPress={handleContinue}>
+                <Text style={styles.continueButtonText}>{language === 'es' ? 'Continuar' : 'Continue'}</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </Modal>
+
+      <View style={styles.adContainer}>
+        <Text style={styles.adText}>
+          {translations[language].adSpace}
+        </Text>
+      </View>
+    </View>
   );
 };
 
@@ -240,7 +233,7 @@ const styles = StyleSheet.create({
     padding: 20,
     backgroundColor: '#f9f9f9',
     borderRadius: 10,
-    marginBottom: 20,
+    marginBottom: 40,
     width: '100%',
   },
   darkInstructionBox: {
@@ -269,7 +262,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#007BFF',
     padding: 15,
     borderRadius: 5,
-    marginBottom: 20,
+    marginBottom: -20,
   },
   darkButton: {
     backgroundColor: '#0056b3',
@@ -283,7 +276,7 @@ const styles = StyleSheet.create({
   },
   settingsButton: {
     position: 'absolute',
-    top: 20,
+    top: 40,
     right: 20,
   },
   settingsIcon: {
@@ -404,6 +397,7 @@ const styles = StyleSheet.create({
   },
   adContainer: {
     width: '100%',
+    height: 70,
     padding: 10,
     backgroundColor: '#f8f8f8',
     alignItems: 'center',
@@ -411,7 +405,7 @@ const styles = StyleSheet.create({
     borderTopWidth: 1,
   },
   adText: {
-    fontSize: 14,
+    fontSize: 20,
   },
 });
 
